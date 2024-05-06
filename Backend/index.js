7705a3371a8f5204bfe81ba1b8481fce6e7a1e75
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-05-06 23:00:07 +0530

    Server File Updated with Middlewares

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,18 @@
-const express=require("express");
+const express = require("express");
+const connectDatabase = require("./config/Database");
+const userRoutes=require("./routes/User");
+const cookieParser=require("cookie-parser");
 require("dotenv").config();
 
-const app=express();
-const PORT=process.env.PORT || 4445;
+const app = express();
+const PORT = process.env.PORT || 4445;
 
-app.listen(PORT, ()=>{
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/user", userRoutes)
+
+app.listen(PORT, () => {
+    connectDatabase();
     console.log(`Server started at PORT: ${PORT}`);
 })
