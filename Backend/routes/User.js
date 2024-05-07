1b8d9c92d5ab8ee0bc31b5e3d0b28cafd5518f21
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-05-07 15:18:58 +0530

    Logout Routes Created

--- Backend/routes/User.js
+++ Backend/routes/User.js
@@ -1,8 +1,9 @@
 const express=require("express");
-const { signupUser, loginUser } = require("../controller/User");
+const { signupUser, loginUser, logoutUser } = require("../controller/User");
 const router=express.Router();
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
+router.post("/logout", logoutUser);
 
 module.exports=router
